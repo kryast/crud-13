d981d6f37c947a1493e686f5163e13febb95990d
Ahmad Syarifuddin <oden.psht@gmail.com>
2025-08-10 08:12:11 +0700

    Setup Database

---
+++ database/postgres.go
@@ -0,0 +1,12 @@
+package database
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func InitDB() (*gorm.DB, error) {
+	dsn := "host=localhost user=postgres password=postgres dbname=crud13 port=5432 sslmode=disable TimeZone=Asia/Jakarta"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	return db, err
+}
