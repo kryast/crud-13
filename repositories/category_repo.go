763c1195a004d3afee4155e2e7d80da6d043d328
Ahmad Syarifuddin <oden.psht@gmail.com>
2025-08-10 08:20:52 +0700

    Create Category Repository

---
+++ repositories/category_repo.go
@@ -0,0 +1,14 @@
+package repositories
+
+import "gorm.io/gorm"
+
+type CategoryRepository interface {
+}
+
+type categoryRepository struct {
+	db *gorm.DB
+}
+
+func NewCategoryRepository(db *gorm.DB) CategoryRepository {
+	return &categoryRepository{db}
+}
